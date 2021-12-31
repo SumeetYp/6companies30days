16aa77345e7b9e6af428c0d7b7226ff1bb420a8f
fiercespartan <loharrohit9860@gmail.com>
2021-12-31 15:22:31 +0530

    first day\

--- goldmanprblm_1.java
+++ goldmanprblm_1.java
@@ -6,7 +6,7 @@
 import java.io.*;
 import java.util.*;
 
-class Asign1 {
+class goldmanprblm_1 {
     public static void main (String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	        
